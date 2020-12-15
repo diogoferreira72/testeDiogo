cb8857156b1eed63cbe68da6da62801dd693c831
DiogoPereira <diogoferreira72@gmail.com>
2020-12-15 10:27:51 -0300

    alteracoes

--- Interface.java
+++ Interface.java
@@ -4,7 +4,8 @@
 public class Interface {
 
     public static void main(String[] args) {
-            
+            System.out.println("Hello World!");
+            System.out.println("Alternativas de commit");
         
     }
 }
