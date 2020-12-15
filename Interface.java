8cc38f5c169de06fb5290d1c284ce1bd984f5b9f
Diogo Ferreira <diogoferreira72@hotmail.com>
2020-12-15 10:29:46 -0300

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -6,6 +6,9 @@ public class Interface {
     public static void main(String[] args) {
             System.out.println("Hello World!");
             System.out.println("Alternativas de commit");
+            System.out.println("Alterações feitas dentro do Git");
+                 
+       
         
     }
 }
